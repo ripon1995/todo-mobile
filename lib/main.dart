6250cd93cd1854769b39081e504ad294d2bd1e47
Ripon <alamin.ripon@brainstation-23.com>
2023-06-24 14:46:45 +0600

    Intial binding added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/app/bindings/initial_binding.dart';
 import 'package:flutter_basic/app/data/local/preference/preference_manager.dart';
 import 'package:flutter_basic/app/data/local/preference/preference_manager_impl.dart';
-import 'package:flutter_basic/app/modules/profile/controllers/profile_controller.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 late PreferenceManager _preferenceManager;
-late ProfileController _profileController;
 
 void main_app() {
   _preferenceManager = Get.put(PreferenceManagerImpl());
-  _profileController = Get.put(ProfileController());
 
   runApp(
     GetMaterialApp(
       title: "Application",
+      initialBinding: InitialBinding(),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       color: Colors.red,
