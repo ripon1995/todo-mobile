6d450aa9f06e7ca122d1396e1b97476d36e5b6c8
Ripon <alamin.ripon@brainstation-23.com>
2023-07-05 14:00:20 +0600

    Refactor - deleted commented code

--- lib/app/log.dart
+++ lib/app/log.dart
@@ -11,7 +11,7 @@ class Log {
             lineLength: 120, // width of the output
             colors: true, // Colorful log messages
             printEmojis: true, // Print an emoji for each log message
-            printTime: false // Should each log print contain a timestam
+            printTime: false // Should each log print contain a timestamp
             ));
   }
 
