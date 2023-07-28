7b1eadcba5dc20282dfb36ce173bb4b86d9fef65
Ripon <alamin.ripon@brainstation-23.com>
2023-07-28 14:46:44 +0600

    user profile picture
    what?
    - home view ui update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -28,34 +28,45 @@ class HomeView extends GetView<HomeController> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Obx(
-              () => SizedBox(
-                height: 120,
-                child: ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    ...List.generate(
-                      controller.rxToDoList.length,
-                      (index) => listItem(
-                        context,
-                        controller.rxToDoList[index],
-                        controller,
-                        index,
+            Container(
+              margin: const EdgeInsets.only(top: 50, left: 5),
+              child: Column(
+                children: [
+                  Obx(
+                    () => SizedBox(
+                      height: 120,
+                      child: ListView(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          ...List.generate(
+                            controller.rxToDoList.length,
+                            (index) => listItem(
+                              context,
+                              controller.rxToDoList[index],
+                              controller,
+                              index,
+                            ),
+                          ),
+                          if (controller.rxToDoListNext.isNotEmpty)
+                            IconButton(
+                              onPressed: controller.getToDoList,
+                              icon: const Icon(
+                                Icons.arrow_forward_ios_rounded,
+                              ),
+                            )
+                        ],
                       ),
                     ),
-                    if (controller.rxToDoListNext.isNotEmpty)
-                      IconButton(
-                        onPressed: controller.getToDoList,
-                        icon: const Icon(
-                          Icons.arrow_forward_ios_rounded,
-                        ),
-                      )
-                  ],
-                ),
+                  ),
+                ],
               ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: _profileIcon(),
             ),
           ],
         ),
